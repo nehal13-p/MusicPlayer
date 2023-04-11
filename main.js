1bee2cb2924122c0b9238edcc6b5e88d954dbbc6
nehal13-p <82352013+nehal13-p@users.noreply.github.com>
2023-04-11 22:53:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//Head Section
+<>//Head Section
+    <link rel="stylesheet" href="style.css" /><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    //Body Section
+    <script src="app.js"></script></>
